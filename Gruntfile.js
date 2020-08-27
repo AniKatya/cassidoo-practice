9ceeb6a3983f4078bf82c765e08861d47e25369e
Anikatya <lopukhina.ekaterina@gmail.com>
2020-08-27 09:22:37 +0300

    added grunt for watching and uglifying

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    name: "class",
+    watch: {
+      files: ["*.js", "*.css"],
+      task: ["updated"],
+    },
+    uglify: {
+      build: {
+        src: ["index.js", "logger.js"],
+        dest: "dist/bundle.js",
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  grunt.registerTask("updated", () => {
+    grunt.log.writeln("It's updated again");
+  });
+
+  grunt.registerTask("default", () => {
+    grunt.log.writeln(`${grunt.config.get("name")}`);
+  });
+};
